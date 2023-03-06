cdd530936ab91bf483726aae09923a02b1560ec7
Claudio Metelli <metelli.claudio@gmail.com>
2023-03-06 12:28:06 +0100

    initialized app and gitignore

---
+++ backend/index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const http = require("http");
+
+const PORT = 3000;
+const app = express();
+const server = http.createServer(app);
+
+server.listen(PORT, () => {
+    console.log(`Server is running on port: ${PORT}`);
+});
