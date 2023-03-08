02f0146a6655039cc8c21c7588dfc5a50bb01d6d
Claudio Metelli <metelli.claudio@gmail.com>
2023-03-08 15:56:26 +0100

    added patch and control error

--- backend/database/dbReader.js
+++ backend/database/dbReader.js
@@ -3,12 +3,12 @@ import { JSONFileSync } from "lowdb/node";
 
 const userAdapter = new JSONFileSync("./../database/users.json");
 export const users = new LowSync(userAdapter);
-users.read();
+users.read().catch((error) => console.error(`An error has occurred:${error}`));
 
 const postAdapter = new JSONFileSync("./../database/posts.json");
 export const posts = new LowSync(postAdapter);
-posts.read();
+posts.read().catch((error) => console.error(`An error has occurred:${error}`));
 
 const commentAdapter = new JSONFileSync("./../database/comments.json");
 export const comments = new LowSync(commentAdapter);
-comments.read();
+comments.read().catch((error) => console.error(`An error has occurred:${error}`));
--- backend/routes/postRoute.js
+++ backend/routes/postRoute.js
@@ -40,8 +40,10 @@ router.post("/", (req, res) => {
         body: postBody,
     };
     posts.data.push(post);
-    posts.write();
-    res.status(201).send("Post succesfully created");
+    posts
+        .write()
+        .then(res.status(201).send("Post succesfully created"))
+        .catch((error) => console.error(`An error has occurred:${error}`));
 });
 
 router.put("/:id", (req, res) => {
@@ -57,8 +59,29 @@ router.put("/:id", (req, res) => {
             post.body = postBody;
         }
     });
-    posts.write();
-    res.status(204).send("Post succesfully updated");
+    posts
+        .write()
+        .then(res.status(204).send())
+        .catch((error) => console.error(`An error has occurred:${error}`));
+});
+
+router.patch("/:id", (req, res) => {
+    const postId = req.params.id;
+    const postTitle = req.body.title;
+    const postBody = req.body.body;
+    if (posts.data.filter((post) => post.id == postId).length === 0) {
+        res.status(404).send("Post not Found");
+    }
+    posts.data.map((post) => {
+        if (post.id == postId) {
+            if (postTitle) post.title = postTitle;
+            if (postBody) post.body = postBody;
+        }
+    });
+    posts
+        .write()
+        .then(res.status(204).send())
+        .catch((error) => console.error(`An error has occurred:${error}`));
 });
 
 export default router;
