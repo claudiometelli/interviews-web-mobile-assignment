6067418107213be023203b6be0d6284af264fc80
Claudio Metelli <metelli.claudio@gmail.com>
2023-03-08 15:18:16 +0100

    added update for posts

--- backend/routes/postRoute.js
+++ backend/routes/postRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { posts, comments } from "../database/dbReader.js";
+import { users, posts, comments } from "../database/dbReader.js";
 
 const router = express.Router();
 
@@ -11,8 +11,7 @@ router.get("/:id", (req, res) => {
     const postId = req.params.id;
     const result = posts.data.find((post) => post.id == postId);
     if (result === undefined) {
-        res.status(404);
-        res.send("Post not Found");
+        res.status(404).send("Post not Found");
     }
     res.json(result);
 });
@@ -20,8 +19,7 @@ router.get("/:id", (req, res) => {
 router.get("/:id/comments", (req, res) => {
     const postId = req.params.id;
     if (posts.data.filter((post) => post.id == postId).length === 0) {
-        res.status(404);
-        res.send("Post not Found");
+        res.status(404).send("Post not Found");
     }
     const result = comments.data.filter((comment) => comment.postId == postId);
     res.json(result);
@@ -31,6 +29,9 @@ router.post("/", (req, res) => {
     const postTitle = req.body.title;
     const postBody = req.body.body;
     const postUser = req.body.user;
+    if (users.data.filter((user) => user.id == postUser).length === 0) {
+        res.status(404).send("User not Found");
+    }
     const maxId = posts.data.length ? Math.max(...posts.data.map((post) => post.id)) : 0;
     const post = {
         userId: postUser,
@@ -40,7 +41,24 @@ router.post("/", (req, res) => {
     };
     posts.data.push(post);
     posts.write();
-    res.json(post);
+    res.status(201).send("Post succesfully created");
+});
+
+router.put("/:id", (req, res) => {
+    const postId = req.params.id;
+    const postTitle = req.body.title;
+    const postBody = req.body.body;
+    if (posts.data.filter((post) => post.id == postId).length === 0) {
+        res.status(404).send("Post not Found");
+    }
+    posts.data.map((post) => {
+        if (post.id == postId) {
+            post.title = postTitle;
+            post.body = postBody;
+        }
+    });
+    posts.write();
+    res.status(204).send("Post succesfully updated");
 });
 
 export default router;
